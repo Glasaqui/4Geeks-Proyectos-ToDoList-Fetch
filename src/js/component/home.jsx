86e7518cfac0a5be18d839619e99555f07a68bc2
Glasaqui <gsanchezquiroz@outlook.com>
2022-09-15 18:01:35 +0000

    Actualizar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,78 @@
 import React from "react";
+import { Input} from "reactstrap";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const[Todolist,setTodoList] = useState([]);
+    const[nuevaTask,setnuevaTask] = useState(" ");
+	const handleChange =(evento) => {
+		setnuevaTask(evento.target.value);
+	};
+	const handlerKeypress =(evento) => {
+		//event.preventDefault();
+		if(evento.key=='Enter') {
+			
+			if(nuevaTask!=' '){
+				
+				setTodoList([...Todolist,nuevaTask])
+				setnuevaTask(' ')
+			}
+		}
+	};
+	const borrar =(id) => {
+		const listafiltrada = Todolist.filter((e,index) => index !==id); 
+		setTodoList(listafiltrada);
+	};
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+			<h1>ToDo List</h1>
+			
+			<div className="row">
+				<div className="col-md-6">
+					<div className="card card-white">
+						<div className="card-body">
+
+						<div className="todo-list">
+                        	<div className="todo-item">
+                            	
+									<Input 
+									type="text"
+									placeholder="Tarea por hacer"
+									className="form-control"
+									id="floatingInput"
+									onChange={handleChange} 
+									value={nuevaTask}
+									onKeyDown={handlerKeypress}
+									/>
+									
+									{Todolist.map((tarea,i)=>{
+										return(
+											<div className="todo-list">
+												<div className="todo-item">
+													
+													<span key={i}>{tarea}
+													</span>
+													
+													<button className="add-btn"  on  onClick={( )=> borrar(i)}>X</button>		
+												</div>
+											</div>
+										)
+									})
+									}	
+									
+									</div>
+									</div>
+								
+					</div>
+				</div>
+			</div>
+		</div>	
+    </div>
+  
 	);
 };
 
