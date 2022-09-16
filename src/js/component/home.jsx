e1ce4e50a943473cc62af5a480dbed0b32a6a1ee
Glasaqui <gsanchezquiroz@outlook.com>
2022-09-16 03:33:20 +0000

    Fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,87 @@
 import React from "react";
 import { Input} from "reactstrap";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	const[Todolist,setTodoList] = useState([]);
     const[nuevaTask,setnuevaTask] = useState(" ");
+
+	
+
+	const GURL = "https://assets.breatheco.de/apis/fake/todos/user/glasaqui";
+    
+    const INI_POST = {
+        method: "POST",
+        body: JSON.stringify(Todolist),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    
+    const DEL_DELETE = {                                                                                                                
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+      
+	      //Carga la lista guardada previamente 
+		  const getApilist = async () =>{
+
+			let resp = await fetch(GURL);
+			if(resp.ok){
+				let APIList = await resp.json();
+				setTodoList(APIList);
+				setnuevaTask("");
+				console.log("Lista guardada cargada con exito")
+				}else{
+				let createuser = await fetch(GURL , INI_POST)
+				if(createuser.ok){
+					console.log("usuario iniciado con exito");
+				}
+			}
+			return console.log("iniciado")
+		}
+	
 	const handleChange =(evento) => {
 		setnuevaTask(evento.target.value);
 	};
-	const handlerKeypress =(evento) => {
+	const handlerKeypress = async (evento) => {
 		//event.preventDefault();
 		if(evento.key=='Enter') {
 			
 			if(nuevaTask!=' '){
 				
 				setTodoList([...Todolist,nuevaTask])
-				setnuevaTask(' ')
+				
+				let newList = Todolist;     
+            
+            	newList.push({
+                label: nuevaTask,
+                done: false
+            });
+
+			let response = await fetch(GURL, {
+                method: "PUT",
+                body: JSON.stringify(newList),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+
+			if (response.ok) {
+                let resp = await fetch(GURL);
+                let APIList = await resp.json();
+                setTodoList(APIList);
+                setnuevaTask(' ');
+            } else {
+                alert("intenta de nuevo, tienes un error");
+            }           
+				
 			}
 		}
 	};
@@ -27,7 +89,10 @@ const Home = () => {
 		const listafiltrada = Todolist.filter((e,index) => index !==id); 
 		setTodoList(listafiltrada);
 	};
-	return (
+
+	    
+		  
+  	return (
 		<div className="text-center">
 			<h1>ToDo List</h1>
 			
@@ -71,6 +136,7 @@ const Home = () => {
 				</div>
 			</div>
 		</div>	
+		
     </div>
   
 	);
